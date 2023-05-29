d05b19b5e1a078f56c91336b6c2fcdf1d11bc956
yeonghak <yongxue1215@126.com>
2023-05-29 09:20:55 +0800

    test: add baidu tongji

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,27 @@ import { useEffect } from 'react'
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
+import { Router } from 'next/router'
+Router.events.on('routeChangeComplete', (url) => {
+  try {
+    window._hmt.push(['_trackPageview', url])
+  } catch (e) {}
+})
+
 export default function App({ Component, pageProps }) {
+  const getAnalyticsTag = () => {
+    return {
+      __html: `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?8f8def5f99b256f42f3354be5d24307e";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();`,
+    }
+  }
+
   useEffect(() => {
     function enableGoogleAdsense() {
       const head = document.getElementsByTagName('head')[0]
@@ -35,6 +55,7 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <script dangerouslySetInnerHTML={getAnalyticsTag()} />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
