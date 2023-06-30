ba44663987e3b21acb33f6aeff134f606b069f8b
yeonghak <yongxue1215@126.com>
2023-06-30 13:40:17 +0800

    edit menu

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
+  { href: 'https://www.duluwa.com', title: '朝鲜族网站' },
   { href: '/blog', title: '文章' },
   { href: '/tags', title: '标签' },
-  { href: 'https://www.duluwa.com', title: '导航' },
   // { href: '/projects', title: 'Projects' },
   // { href: '/about', title: 'About' },
 ]
